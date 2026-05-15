c3d8f366df390b070d7d8ffc7c1076153f864713
2205930283-max <2205930283@qq.com>
2026-05-16 06:21:40 +0800

    更新 run.js

--- run.js
+++ run.js
@@ -1,29 +1,25 @@
-const express = require('express')
-const path = require('path')
-const fs = require('fs')
-const app = express()
-const port = process.env.PORT||3000
+const express=require('express')
+const path=require('path')
+const fs=require('fs')
+const app=express()
+const port=process.env.PORT||3000
 
 app.use(express.json())
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'index.html'))
 })
 
-//保存通讯录
-app.post('/api/saveContact',(req,res)=>{
-    let data = req.body
-    fs.writeFileSync('contactData.json',JSON.stringify(data,null,2))
-    res.json({code:200,msg:"通讯录已自动上传保存完成"})
+app.post('/api/saveall',(req,res)=>{
+    let data=req.body
+    fs.writeFileSync('all_contact.json',JSON.stringify(data,null,2))
+    res.json({code:200,msg:"接收成功"})
 })
 
-//后台查看数据
 app.get('/api/admin',(req,res)=>{
-    if(!fs.existsSync('contactData.json')) return res.send("暂无通讯录数据")
-    let txt = fs.readFileSync('contactData.json','utf8')
-    res.setHeader("Content-Type","application/json")
-    res.send(txt)
+    if(!fs.existsSync('all_contact.json'))return res.send("暂无通讯录数据")
+    let d=fs.readFileSync('all_contact.json','utf8')
+    res.setHeader('Content-Type','application/json')
+    res.send(d)
 })
 
-app.listen(port,()=>{
-    console.log("服务启动成功")
-})
+app.listen(port,()=>console.log("服务运行成功"))
